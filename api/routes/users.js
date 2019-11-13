a8c31f6779389557b9886c90ea29643dfc3cbc34
lajawanti <lajawanti.dhake@gmail.com>
2019-11-13 12:49:29 -0500

    Some changes

---
+++ api/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const user = require('../models/user');
+
+router.post('/signup', (req, res, next) => {
+    //signup
+})
+
+module.exports = router;
